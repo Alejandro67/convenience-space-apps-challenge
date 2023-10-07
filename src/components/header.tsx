cad02219d343e8c5ba989d21e4796ab10f0cde51
111090-TomasCorsico <111090@tecnicatura.frc.utn.edu.ar>
2023-10-07 13:48:21 -0300

    Header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,32 @@
-import Link from "next/link";
+import React from 'react';
+import { Box, Flex, Link, Heading, Button, Spacer, Avatar } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const Header: React.FC = () => {
     return (
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
+        <Box bg="#49C646" p={4}>
+          <Flex alignItems="center" flexWrap="wrap">
+            <Heading color="white" size="lg">
+              My Next.js App
+            </Heading>
+            <Spacer />
+            <Box>
+              <Heading size="md" color="white" mb={2}>
+                lol
+              </Heading>
+              <Heading size="md" color="white" mb={2}>
+                lol
+              </Heading>
+              
+            </Box>
+            <Box ml={8}> 
+          <Avatar />
+        </Box>
+          </Flex>
+        </Box>
+      );
+    };
+    
+  
   
 export default Header;
