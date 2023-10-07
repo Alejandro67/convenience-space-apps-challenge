974193a43ce17195abc5fb2cd49b43a3164d84fd
111090-TomasCorsico <111090@tecnicatura.frc.utn.edu.ar>
2023-10-07 18:15:29 -0300

    added Category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,10 @@ import {
   Button
 } from "@chakra-ui/react"
 import Header from '@/components/header'
-import CategoryCard from '@/components/categorycard'
+import CategoryCard from '../components/categoryCard'
 import Footer from '@/components/footer'
+import Categories from '../data/categories.json'
+import Category from '@/interfaces/Category'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +21,10 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <CategoryCard category={{description:"", imageUrl:"", name:""}}/>
-            <Button onClick={() => signIn()}>
+      {Categories.map((categoryObject: Category, index: number) => (
+        <CategoryCard key={index} category={categoryObject} />
+      ))}
+        <Button onClick={() => signIn()}>
         LOGGEARSE
       </Button>
       <Footer/>
