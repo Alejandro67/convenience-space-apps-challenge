9632580a728bd0239de8cd05518ef5032b4f9109
111090-TomasCorsico <111090@tecnicatura.frc.utn.edu.ar>
2023-10-07 21:01:05 -0300

    Implemented Repo Listing

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,34 +1,80 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import CategoryCard from "@/components/categoryCard";
- // Ensure the correct import path for your JSON file
+// Ensure the correct import path for your JSON file
 import Category from "@/interfaces/Category";
 import { Box, Flex } from "@chakra-ui/react";
-import { useSearchParams} from "next/navigation";
-import {FaArrowLeft} from "react-icons/fa";
-
+import { useSearchParams } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
+import { useProjects } from "@/provider/ProjectsProvider";
+import { Project } from "@/types/Project";
+import ProjectCard from "@/components/projectCard";
 
 const CategoryPage: React.FC = () => {
-  
-    const searchParams = useSearchParams()
- 
-    const search = searchParams.get('cat')
+  const projectContext = useProjects();
 
-    
- 
-     return (
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get("cat");
+
+  const [filter, setFilter] = useState("");
+  const [projectsObjects, setProjectsObjects] = useState<Project[]>([]);
+  const [backup,setBackup] = useState<Project[]>([])
+  useEffect(() => {
+    const filteredProjects = projectsObjects.filter((project) =>
+      project.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    // Update the filtered categories in state
+
+    if (filter === "") {
+      //@ts-ignore
+      setProjectsObjects(backup);
+    } else {
+      setProjectsObjects(filteredProjects);
+    }
+  }, [filter]);
+
+  useEffect(() => {
+    //@ts-ignore
+    let listProjects = projectContext.state.projects;
+    let formattedListProjects:Project[] = [];
+    console.log(listProjects)
+    listProjects.forEach(project => {
+        if (project.categories.includes(search?.toString())){
+            formattedListProjects.push(project);
+        }
+        setBackup(formattedListProjects)
+    });
+    setProjectsObjects(formattedListProjects);
+
+  }, []);
+
+  const handleInputChange = (value: SetStateAction<string>) => {
+    setFilter(value);
+  };
+
+  return (
     <>
-      <Header />
+      <Header onChangeInput={handleInputChange} />
+      <Flex flexWrap="wrap">
+        {projectsObjects.map((projectObject: Project, index: number) => (
+          <Box
+            key={index}
+            flex={1}
+            flexBasis={`calc(100% / ${3})`} // Calculate the width based on the desired number of columns
+            p={2} // Adjust padding as needed
+          >
+            <ProjectCard project={projectObject} />
+          </Box>
+        ))}
+      </Flex>
       
-      <h1>{search}</h1>
       <Footer />
     </>
   );
-
-  
-  
 };
 
 export default CategoryPage;
